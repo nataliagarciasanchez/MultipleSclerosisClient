7a851b13182ae5ea9db3ba731c6e0e61e0d8a89f
Maite Gomez del Rio <maite.gomezriovinuesa@usp.ceu.es>
2024-10-30 20:57:58 +0100

    Modification and rearegement of packages

--- src/main/java/Menu/ClientMenu.java
+++ src/main/java/Menu/ClientMenu.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package multiplesclerosisclient;
+package Menu;
 
 /**
  *
- * @author nataliagarciasanchez
+ * @author maipa
  */
-public class Client {
-    //test Client
+public class ClientMenu {
+    
 }
