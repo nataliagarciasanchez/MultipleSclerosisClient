cf5bd55b46a0c525424e70389bd0a4acbb1a195d
andreamtnz <andrea.martinezpalacios@usp.ceu.es>
2024-10-30 22:52:10 +0100

    added relevanr POJOs

---
+++ src/main/java/POJOs/Symptom.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POJOs;
+
+import java.util.Objects;
+import javax.persistence.*;
+
+/**
+ *
+ * @author Andreoti
+ */
+@Entity
+public class Symptom {
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue (generator = "symptoms")
+    @TableGenerator(name = "symptoms", table = "sqlite_sequence",  pkColumnName = "name", valueColumnName = "seq", pkColumnValue = "symptoms")
+    private Integer id;
+    private String name;
+    
+    public Symptom(){
+    super();
+    }
+
+    public Symptom(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Symptom{" + "id=" + id + ", name=" + name + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 61 * hash + Objects.hashCode(this.id);
+        hash = 61 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Symptom other = (Symptom) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return Objects.equals(this.id, other.id);
+    }
+   
+    
+}
